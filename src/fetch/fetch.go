7957e2410dba07816f7b2caef446e49b9140ccb4
Simranpal <simranpal.singh@suse.com>
2020-02-11 15:14:57 -0800

    Add http prefix if missing in url

--- src/fetch/fetch.go
+++ src/fetch/fetch.go
@@ -6,10 +6,16 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		//Add http prefix if missing in url
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
 		//http response fetched in the resp struct, and corrosponding error in err
 		resp, err := http.Get(url)
 		if err != nil {
